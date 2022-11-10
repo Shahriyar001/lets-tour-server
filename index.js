7951377bc4210646d308271337de783fa1659aa0
Shahriyar001 <shahriyarmahbub55@gmail.com>
2022-11-10 06:43:22 +0600

    create servicecollection sector and genarate data from mongodb

--- index.js
+++ index.js
@@ -13,12 +13,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dawimtn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        const serviceCollection = client.db('letsTour').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('tour server is running')
