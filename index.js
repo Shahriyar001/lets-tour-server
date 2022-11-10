477dd3a474298c3978a6890e02b8a165ccca0e86
Shahriyar001 <shahriyarmahbub55@gmail.com>
2022-11-10 11:43:01 +0600

    create post in server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -17,12 +17,27 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('letsTour').collection('services');
+        const reviewCollection = client.db('letsTour').collection('allReview');
 
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        // AllReview api
+        app.post('/allReview', async (req, res) => {
+            const allReview = req.body;
+            const result = await reviewCollection.insertOne(allReview);
+            res.send(result);
         })
     }
     finally {
